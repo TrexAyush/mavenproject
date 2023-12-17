f532bbfb4ec7aa4d5a351fcea145b0fd36a3b00b
TrexAyush <thisisayushtam@gmail.com>
2023-12-17 15:02:43 +0530

    initial commit

---
+++ src/test/java/com/jsp/SpringBootCRUD/SpringBootCrudApplicationTests.java
@@ -0,0 +1,10 @@
+package com.jsp.SpringBootCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudApplicationTests {
+
+
+}
